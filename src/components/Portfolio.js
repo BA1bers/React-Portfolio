a0bd68bbd93686d019ee3315c718ebf7143c4ab0
BA1bers <brent.albers@yahoo.com>
2021-11-29 07:36:22 -0700

    added description and links to github repositories in portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -17,14 +17,44 @@ const Portfolio = () => {
                     <p className="portfolioTopText">{header.text}</p>
                 </div>
                 <div className="row">
-                    <Project title="passwordGenerator" link="https://ba1bers.github.io/PasswordGenerator/" image={PasswordGenerator}/>
+                    <div className="portProjects">
+                    <Project title="passwordGenerator" link="https://ba1bers.github.io/PasswordGenerator/" image={PasswordGenerator} />
+                    <p>Password generator allows you to input the criteria you need for a password and creates one for you<br>
+                    </br>link to github repository <a href="https://github.com/BA1bers/PasswordGenerator" target="_blank" rel="noopener noreferrer">here</a>
+                    </p>
+                    </div>
+                    <div className="portProjects">
                     <Project title="codeQuiz" link="https://ba1bers.github.io/CodeQuiz/" image={CodeQuiz} />
+                    <p>A timed quiz that keeps track of your score. For every question you get wrong, time is deducted from the timer.<br>
+                    </br>link to github repository <a href="https://github.com/BA1bers/CodeQuiz" target="_blank" rel="noopener noreferrer">here</a>
+                    </p>
+                    </div>
+                    <div className="portProjects">
                     <Project title="dayScheduler" link="https://ba1bers.github.io/Scheduler/" image={DayScheduler}/>
+                    <p>A daily planner where you can mark down important events<br>
+                    </br>link to github repository <a href="https://github.com/BA1bers/Scheduler" target="_blank" rel="noopener noreferrer">here</a>
+                    </p>
+                    </div>
                 </div>
                 <div className="row">
+                    <div className="portProjects">
                     <Project title="weatherDashboard" link="https://ba1bers.github.io/weather-dashboard/" image={WeatherDashboard} />
+                    <p>Allows you to search any city and it will retrieve the forecast, temperature, wind speed, and U.V. index for the next 5 days<br>
+                    </br>link to github repository <a href="https://github.com/BA1bers/weather-dashboard" target="_blank" rel="noopener noreferrer">here</a>
+                    </p>
+                    </div>
+                    <div className="portProjects">
                     <Project title="noteTaker" link="https://mighty-cove-49648.herokuapp.com/" image={NoteTaker} />
+                    <p>A simple note taking app to save thoughts and keep track of tasks that need to be completed<br>
+                    </br>link to github repository <a href="https://github.com/BA1bers/Note-Taker" target="_blank" rel="noopener noreferrer">here</a>
+                    </p>
+                    </div>
+                    <div className="portProjects">
                     <Project title="mernlin" link="https://mernlin-pizza.herokuapp.com/Hero" image={Mernlin} />
+                    <p>MERNlin's Pizza is an application built for a restaurant order system. It allows users to create account to receive discounts by becoming a member<br>
+                    </br>link to github repository <a href="https://github.com/BA1bers/Mernlin-Pizza" target="_blank" rel="noopener noreferrer">here</a>
+                    </p>
+                    </div>
                 </div>
             </div>
         </div>
